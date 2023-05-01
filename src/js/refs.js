c3c09fa0230e4839ace0e2e2a2a85a411a64a5fc
Cupez04 <selop0639@gmail.com>
2023-05-01 17:35:56 -0500

    tarea 11

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+const formRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadRef = document.querySelector('.js-load-btn');
+
+
+export {formRef, galleryRef, loadRef};
